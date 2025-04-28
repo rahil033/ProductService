ea212c7d90a6fa971cd4b724082c823a1e311e64
rahilahmed <rahmed033@gmail.com>
2025-04-28 22:18:40 +0530

    Established DB connection

--- src/main/java/com/rahil/productservice/models/Product.java
+++ src/main/java/com/rahil/productservice/models/Product.java
@@ -1,12 +1,16 @@
 package com.rahil.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private double price;
     private String title;
+    @ManyToOne
     private Category category;
 }
